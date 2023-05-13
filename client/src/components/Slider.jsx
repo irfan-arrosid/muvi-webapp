098061773adeed47582f8639998d91ddf9af1a45
irfan-arrosid <irfanarrosid4@gmail.com>
2023-05-14 00:23:39 +0700

    Feat: add slider to show trending movie list

---
+++ client/src/components/Slider.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import GlobalApi from '../services/GlobalApi'
+
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original'
+
+function Slider() {
+    const [movieList, setMovieList] = useState([])
+
+    useEffect(() => {
+        getTrendingMovies()
+    }, [])
+
+    const getTrendingMovies = () => {
+        GlobalApi.getTrendingVideos.then(res => {
+            console.log(res.data.results)
+            setMovieList(res.data.results)
+        })
+    }
+
+  return (
+    <div className='flex overflow-x-auto w-full px-16 py-4'>
+        {movieList.map((item) => (
+            <img src={IMAGE_BASE_URL+item.backdrop_path} className='min-w-full h-[310px] object-cover object-left-top mr-5 rounded-md' />
+        ))}
+    </div>
+  )
+}
+
+export default Slider
--- client/src/services/GlobalApi.jsx
+++ client/src/services/GlobalApi.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const movieBaseUrl='https://api.themoviedb.org/3'
 const api_key='4be90a2507eca9c973a9b745c903adab'
 
-const getTrendingVideos=axios.get(movieBaseUrl+'/trending/all/day?api_key'+api_key)
+const getTrendingVideos=axios.get(movieBaseUrl+'/trending/all/day?api_key='+api_key)
 
 export default {
     getTrendingVideos
